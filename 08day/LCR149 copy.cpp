e9c7a7a594b916138361aebbe216100454a0cda7
gw <932150283@qq.com>
2024-02-10 22:23:46 +0800

    leetcode-08-2/3

--- 08day/LCR149 copy.cpp
+++ 08day/LCR149 copy.cpp
@@ -1,12 +1,19 @@
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
+
  */
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
 public:
     vector<int> levelOrder(TreeNode* root) {
