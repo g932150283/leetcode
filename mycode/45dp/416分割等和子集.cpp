6f36706d116d2c1720da705dafbfb135a02dc5a5
gw <932150283@qq.com>
2024-04-05 21:15:11 +0800

    dp-medium

---
+++ mycode/45dp/416分割等和子集.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        vector<int> dp(10001, 0);
+        for(int i = 0; i < nums.size(); i++){
+            sum += nums[i];
+        }
+        if(sum % 2 == 1){
+            return false;
+        }
+        int target = sum / 2;
+
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = target; j >= nums[i]; j--){
+                dp[j] = max(dp[j], dp[j - nums[i]] + nums[i]);
+            }
+        }
+        if(dp[target] == target){
+            return true;
+        }
+        return false;
+    }
+};
