d503a9848f1d00a995b643d653fd199544ea887d
gw <932150283@qq.com>
2024-04-11 21:18:19 +0800

    dp-medium

--- mycode/47kama/57爬楼梯.cpp
+++ mycode/47kama/57爬楼梯.cpp
@@ -7,7 +7,7 @@ void getRes(int n, int m){
     vector<int> dp(n + 1, 0);
     dp[0] = 1;
     // 排列数，先遍历背包，再遍历物品
-    for(int i = 0; i <= n; i++){
+    for(int i = 1; i <= n; i++){
         for(int j = 1; j <= m; j++){
             if(i - j >= 0){
                 dp[i] += dp[i - j];
