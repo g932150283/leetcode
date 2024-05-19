4dc49fa220b1d44d027d96f11dced91178a526ba
gw <932150283@qq.com>
2024-05-19 21:50:02 +0800

    tree-medium

--- mycode/hot100/二叉树/114二叉树展开为链表.cpp
+++ mycode/hot100/二叉树/114二叉树展开为链表.cpp
@@ -24,12 +24,59 @@ class Solution {
     }
 public:
     void flatten(TreeNode* root) {
-        preorder(root);
+        // if(root == nullptr){
+        //     return;
+        // }
+        // preorder(root);
+        // TreeNode* cur = root;
+        // for(int i = 1; i < res.size(); i++){
+        //     cur->left = nullptr;
+        //     cur->right = res[i];
+        //     cur = cur->right;
+        // }
         TreeNode* cur = root;
-        for(auto node : res){
-            cur->left = nullptr;
-            cur->right = node;
+        while (cur != nullptr)
+        {
+            /* code */
+            if(cur->left != nullptr){
+                TreeNode* tmp = cur->right;
+                cur->right = cur->left;
+                cur->left = nullptr;
+                TreeNode* cur1 = cur->right;
+                while (cur1->right)
+                {
+                    cur1 = cur1->right;
+                    /* code */
+                }
+                cur1->right = tmp;
+            }
             cur = cur->right;
         }
+        
+        
+        
+        
+            
     }
 };
+
+int main(){
+
+    TreeNode* t1 = new TreeNode(1);
+    TreeNode* t2 = new TreeNode(2);
+    TreeNode* t3 = new TreeNode(3);
+    TreeNode* t4 = new TreeNode(4);
+    TreeNode* t5 = new TreeNode(5);
+    TreeNode* t6 = new TreeNode(6);
+    Solution* s = new Solution();
+    t1->left = t2;
+    t1->right = t5;
+    t2->left = t3;
+    t2->right = t4;
+    t5->right = t6;
+    // t1->left = t2;
+    // t2->left = t3;
+    s->flatten(t1);
+
+    return 0;
+}
