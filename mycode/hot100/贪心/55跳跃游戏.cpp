64c9245451eba7262a257fab20c88ee57e81254d
gw <932150283@qq.com>
2024-06-27 17:47:23 +0800

    greedy-medium

---
+++ mycode/hot100/贪心/55跳跃游戏.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int mx = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(i > mx){
+                return false;
+            }
+            mx = max(mx, i + nums[i]);
+        }
+        return true;
+    }
+};
