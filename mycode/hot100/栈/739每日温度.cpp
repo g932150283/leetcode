822a88b95ec10cc32d53a7181550d541096b142e
gw <932150283@qq.com>
2024-07-03 21:44:37 +0800

    stack-medium

---
+++ mycode/hot100/栈/739每日温度.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int> &temperatures) {
+        int n = temperatures.size();
+        vector<int> ans(n);
+        stack<int> st;
+        for (int i = n - 1; i >= 0; i--) {
+            int t = temperatures[i];
+            while (!st.empty() && t >= temperatures[st.top()]) {
+                st.pop();
+            }
+            if (!st.empty()) {
+                ans[i] = st.top() - i;
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
