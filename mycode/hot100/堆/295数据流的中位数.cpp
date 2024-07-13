581ac18edc43e313f148ed0ee54b744ce02d0e52
gw <932150283@qq.com>
2024-07-13 22:02:27 +0800

    heap-hard

---
+++ mycode/hot100/堆/295数据流的中位数.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class MedianFinder {
+public:
+    priority_queue<int, vector<int>, greater<int>> A; // 小顶堆，保存较大的一半
+    priority_queue<int, vector<int>, less<int>> B; // 大顶堆，保存较小的一半
+    MedianFinder() { }
+    void addNum(int num) {
+        if (A.size() != B.size()) {
+            A.push(num);
+            B.push(A.top());
+            A.pop();
+        } else {
+            B.push(num);
+            A.push(B.top());
+            B.pop();
+        }
+    }
+    double findMedian() {
+        return A.size() != B.size() ? A.top() : (A.top() + B.top()) / 2.0;
+    }
+};
