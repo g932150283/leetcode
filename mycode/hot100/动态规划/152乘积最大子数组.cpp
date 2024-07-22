43a1606329d79f56853426d6a06ef63537676d59
gw <932150283@qq.com>
2024-07-22 19:26:25 +0800

    dp-medium

--- mycode/hot100/动态规划/152乘积最大子数组.cpp
+++ mycode/hot100/动态规划/152乘积最大子数组.cpp
@@ -2,25 +2,16 @@
 
 using namespace std;
 
-
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 0){
-            return 0;
-        } else if(n == 1) {
-            return nums[0];
+        double ans = INT_MIN, mx = 1, mi = 1;
+        for (auto& x : nums)
+        {
+            if (x < 0) swap(mx, mi);
+            mx = max(mx * x, (double)x), mi = min(mi * x, (double)x);
+            ans = max(ans, mx);
         }
-        int p = nums[0];
-        int maxP = nums[0];
-        int minP = nums[0];
-        for(int i = 1; i < n; i++) {
-            int t = maxP;
-            maxP = max(max(maxP * nums[i], nums[i]), minP *nums[i]);
-            minP = min(min(t * nums[i], nums[i]), minP * nums[i]);
-            p = max(maxP, p);
-        }
-        return p;
+        return (int)ans;
     }
 };
