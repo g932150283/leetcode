9b2b266af42d49b683daad22fe0a60d0cbff9688
gw <932150283@qq.com>
2024-08-16 23:41:04 +0800

    tree-next

--- mycode/labuladong/tree/226翻转二叉树.cpp
+++ mycode/labuladong/tree/226翻转二叉树.cpp
@@ -17,11 +17,10 @@ public:
         if(root == nullptr){
             return nullptr;
         }
-        TreeNode* tmp = root->left;
-        root->left = root->right;
-        root->right = tmp;
-        invertTree(root->left);
-        invertTree(root->right);
+        TreeNode* left = invertTree(root->left);
+        TreeNode* right = invertTree(root->right);
+        root->left = right;
+        root->right = left;
         return root;
     }
 };
