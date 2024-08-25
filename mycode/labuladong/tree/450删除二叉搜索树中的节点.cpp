38d146dff85d04e3ef5689884d8fcbc925c202c5
gw <932150283@qq.com>
2024-08-25 23:29:45 +0800

    tree-BST

--- mycode/labuladong/tree/450删除二叉搜索树中的节点.cpp
+++ mycode/labuladong/tree/450删除二叉搜索树中的节点.cpp
@@ -12,17 +12,40 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 class Solution {
+    TreeNode* getMin(TreeNode* root){
+        while (root->left != nullptr)
+        {
+            root = root->left;
+            /* code */
+        }
+        return root;
+        
+    }
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
-    if (root->val == key) {
-        // 找到啦，进行删除
-    } else if (root->val > key) {
-        // 去左子树找
-        root->left = deleteNode(root->left, key);
-    } else if (root->val < key) {
-        // 去右子树找
-        root->right = deleteNode(root->right, key);
+        if(root == nullptr){
+            return nullptr;
+        }
+        if(root->val == key){
+            // delete
+            if(root->left == nullptr){
+                return root->right;
+            }
+            if(root->right == nullptr){
+                return root->left;
+            }
+            TreeNode* minNode = getMin(root->right);
+            root->right = deleteNode(root->right, minNode->val);
+            minNode->left = root->left;
+            minNode->right = root->right;
+            root = minNode;
+        }
+        if(root->val < key){
+            root->right = deleteNode(root->right, key);
+        }
+        if(root->val > key){
+            root->left = deleteNode(root->left, key);
+        }
+        return root;
     }
-    return root;
-}
 };
