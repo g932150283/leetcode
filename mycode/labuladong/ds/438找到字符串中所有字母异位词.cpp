d6ea16dabb370184a1a0348254613e5156098419
gw <932150283@qq.com>
2024-09-16 23:57:37 +0800

    ds-windows

---
+++ mycode/labuladong/ds/438找到字符串中所有字母异位词.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> findAnagrams(string s, string t)
+    {
+        vector<int> res;
+        unordered_map<char, int> needs;
+        unordered_map<char, int> windows;
+        for(char c : t){
+            needs[c]++;
+        }
+        int l = 0, r = 0;
+        int start = 0, minLen = INT_MAX;
+        int match = 0;
+        while (r < s.size())
+        {
+            char c = s[r];
+            if(needs.count(c)){
+                windows[c]++;
+                if(needs[c] == windows[c]){
+                    match++;
+                }
+            }
+            r++;
+            if(match == needs.size()){
+                res.push_back(l);
+                char cc = s[l];
+                if(needs.count(cc)){
+                    windows[cc]--;
+                    if(windows[cc] < needs[cc]){
+                        match--;
+                    }
+                }
+                l++;
+            }
+
+            /* code */
+        }
+        return res;
+        
+    }
+};
